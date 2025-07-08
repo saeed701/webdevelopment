fcc7cb1891b998f0a2d046aa9ecf652ac54664b2
saeed701 <wdevelopment705@gmail.com>
2025-07-08 02:09:05 -0700

    Add files via upload

---
+++ web develment/JAVA SCRIPT CODES/JAVA 3 LOOPS.js
@@ -0,0 +1,66 @@
+// for loop
+// for (let a = 0; a <=100; a++) {
+//     console.log(a);  
+// }
+
+
+//FOR LOOP EXAMPLE 2
+// let sum = 6 ;
+
+// for (let b = 0; b <=5; b++) {
+// sum= sum+b;
+//     console.log("sum",sum,"b",b);
+// }
+
+
+// while loop
+// let c = 0;
+// while (c<10) {
+//     c++;
+//     console.log("apan coll");
+// }
+
+
+//do while loop vs while loop. in do while live my code atleast 1 time run then check condicoimn
+
+//do while loop 
+// let d = 0
+// do {console.log("apna collage",d);
+//     d++;
+// } while (d<=10);
+
+
+//for of loop
+// let e = "codewithharry"
+// let size = 0;
+// for (const f of e) {
+//     console.log(f);
+//     size++;
+// }
+// console.log("string size",size);
+
+
+// for in loop
+// let g = {
+//     name: "omer",
+//     age: 34,
+// }
+// for (const key in g) {
+//     console.log("key", key, "value =", g[key]);
+// }
+
+//prtatics1
+// for (let n = 0; n <= 100; n++) {
+//     if (n % 2 == 0) {
+//         console.log("n",n);
+//     }
+// }
+
+
+//pratcis2
+// let gamenum = 300
+// let usernum = prompt("enter your luckey number")
+// while (gamenum != usernum) {
+// usernum = prompt("you enter wrong your lucky number try another your luvky number")
+// }
+// alert("you win")
