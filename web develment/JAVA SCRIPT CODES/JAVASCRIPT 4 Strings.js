e24be03846889d910cc3606d4bf59c2377881215
saeed701 <wdevelopment705@gmail.com>
2025-07-08 02:25:37 -0700

    Add files via upload

---
+++ web develment/JAVA SCRIPT CODES/JAVASCRIPT 4 Strings.js
@@ -0,0 +1,11 @@
+// strings in a sequence of charactor used to represent text
+let a = "Apna collage"
+a.length
+a=indices
+//3 code APPLY
+console.log(a);
+console.log(a.length);
+console.log(a[5]);
+
+
+
